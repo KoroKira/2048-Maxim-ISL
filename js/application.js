e7c81e31a06c32235697fcaf428b46ed93dd1d36
Guilhem Desarcy-Lemiere <63927791+KoroKira@users.noreply.github.com>
2023-11-28 19:15:13 +0100

    Update application.js

--- js/application.js
+++ js/application.js
@@ -1,7 +1,9 @@
-animationDelay = 150;
-minSearchTime = 100;
+// Définition des délais d'animation et de recherche minimale
+animationDelay = 150; // Délai entre les animations (en millisecondes)
+minSearchTime = 100;   // Temps minimal de recherche (en millisecondes)
 
-// Wait till the browser is ready to render the game (avoids glitches)
+// Attend que le navigateur soit prêt à rendre le jeu (évite les problèmes d'affichage)
 window.requestAnimationFrame(function () {
+  // Crée une instance du gestionnaire de jeu (GameManager) avec le clavier comme gestionnaire d'entrée et HTML comme actuateur
   window.manager = new GameManager(4, KeyboardInputManager, HTMLActuator);
 });
