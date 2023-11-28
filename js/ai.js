e7cf59ce89de8b23cfde5417cebd4b8575f97006
Guilhem Desarcy-Lemiere <63927791+KoroKira@users.noreply.github.com>
2023-11-28 19:17:37 +0100

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -1,108 +1,120 @@
+// Fonction pour obtenir le nom du mouvement en fonction de l'indice
 function moveName(move) {
- return {
-    0: 'up',
-    1: 'right',
-    2: 'down',
-    3: 'left'
+  return {
+    0: 'haut',
+    1: 'droite',
+    2: 'bas',
+    3: 'gauche'
   }[move];
 }
 
-var	global_max_score;
+// Variables globales pour stocker le meilleur score et les mouvements associés
+var global_max_score;
 var global_max_score_moves;
 
+// Fonction pour obtenir le meilleur mouvement en effectuant plusieurs exécutions aléatoires
 function getBestMove(grid, runs, debug) {
-		var bestScore = 0; 
-		var bestMove = -1;
+  var bestScore = 0; 
+  var bestMove = -1;
+  var bestAvgMoves;
 
-		for (var i=0;i<4;i++) {
-			// score move position
-			var res = multiRandomRun(grid, i, runs);
-			var score = res.score;
-			
-			if (score >= bestScore) {
-				bestScore = score;
-				bestMove = i;
-				bestAvgMoves = res.avg_moves;
-			}
-			
-			if (debug) {
-				console.log('Move ' + moveName(i) + ": Extra score - " + score);
-			}
-		}
-		if(!grid.movesAvailable()) console.log('bug2');		
-		// assert move found		
-		if (bestMove == -1) {
-			console.log('ERROR...'); 
-			errorGrid = grid.clone();
-		} 
-		
-		console.log('Move ' + moveName(bestMove) + ": Extra score - " + bestScore + " Avg number of moves " + bestAvgMoves);			
-		
-		return {move: bestMove, score: bestScore};
+  // Parcours des quatre mouvements possibles
+  for (var i = 0; i < 4; i++) {
+    // Score du mouvement pour une position donnée
+    var res = multiRandomRun(grid, i, runs);
+    var score = res.score;
+    
+    // Met à jour le meilleur score et le meilleur mouvement si nécessaire
+    if (score >= bestScore) {
+      bestScore = score;
+      bestMove = i;
+      bestAvgMoves = res.avg_moves;
+    }
+    
+    // Affichage du score supplémentaire si le mode débogage est activé
+    if (debug) {
+      console.log('Mouvement ' + moveName(i) + ": Score supplémentaire - " + score);
+    }
+  }
+
+  // Vérification des mouvements disponibles dans la grille (débogage)
+  if (!grid.movesAvailable()) console.log('bug2');    
+    
+  // Assertion : un mouvement a été trouvé
+  if (bestMove == -1) {
+    console.log('ERREUR...'); 
+    errorGrid = grid.clone();
+  } 
+
+  // Affiche les détails du meilleur mouvement
+  console.log('Mouvement ' + moveName(bestMove) + ": Score supplémentaire - " + bestScore + " Nombre moyen de mouvements " + bestAvgMoves);        
+
+  return {move: bestMove, score: bestScore};
 }
 
-
-
+// Fonction pour effectuer plusieurs exécutions aléatoires et retourner la moyenne des scores et le nombre moyen de mouvements
 function multiRandomRun(grid, move, runs) {
-	var total = 0.0;
-	var min = 1000000;
-	var max = 0;
-	var total_moves = 0;
-	
-	for (var i=0 ; i < runs ; i++) {
-		var res = randomRun(grid, move);
-		var s = res.score;
-		if (s == -1) return -1;
-			
-		total += s;
-		total_moves += res.moves;
-		if (s < min) min = s;
-		if (s > max) max = s;
-	}
-	
-	var avg = total / runs;
-	var avg_moves = total_moves / runs;
+  var total = 0.0;
+  var min = 1000000;
+  var max = 0;
+  var total_moves = 0;
+  
+  // Boucle sur le nombre d'exécutions spécifié
+  for (var i = 0; i < runs; i++) {
+    var res = randomRun(grid, move);
+    var s = res.score;
+    if (s == -1) return -1;
+      
+    total += s;
+    total_moves += res.moves;
+    if (s < min) min = s;
+    if (s > max) max = s;
+  }
+  
+  var avg = total / runs;
+  var avg_moves = total_moves / runs;
 
-//	return max;
-//	return min;
-//	return avg+max;
-	return {score: avg, avg_moves:avg_moves};
+  // Retourne la moyenne des scores et le nombre moyen de mouvements
+  return {score: avg, avg_moves: avg_moves};
 }
 
-function randomRun(grid, move) {	
-	var g = grid.clone();
-	var score = 0;
-	var res = moveAndAddRandomTiles(g, move);
-	if (!res.moved) {
-		return -1; // can't start
-	}	
-	score += res.score;
+// Fonction pour effectuer une exécution aléatoire
+function randomRun(grid, move) {  
+  var g = grid.clone();
+  var score = 0;
+  var res = moveAndAddRandomTiles(g, move);
+  
+  // Vérifie si le mouvement initial a été effectué
+  if (!res.moved) {
+    return -1; // Ne peut pas commencer
+  }  
+  score += res.score;
 
-	// run til we can't
-	var moves=1;
-	while (true) {
-		if (!g.movesAvailable()) break;
-		
-		var res = g.move(Math.floor(Math.random() * 4));
-		if (!res.moved) continue;
-		
-		score += res.score;
-		g.addRandomTile();
-		moves++;
-	}
-	// grid done.
-	return {score:score, moves:moves};
+  // Exécution jusqu'à ce qu'il n'y ait plus de mouvements disponibles
+  var moves = 1;
+  while (true) {
+    if (!g.movesAvailable()) break;
+    
+    var res = g.move(Math.floor(Math.random() * 4));
+    if (!res.moved) continue;
+    
+    score += res.score;
+    g.addRandomTile();
+    moves++;
+  }
+  // Grille terminée.
+  return {score: score, moves: moves};
 }
 
+// Fonction pour effectuer un mouvement et ajouter des tuiles aléatoires
 function moveAndAddRandomTiles(grid, direction) {
-	var res = grid.move(direction);
-	if (res.moved) grid.addRandomTile();
-	return res;
+  var res = grid.move(direction);
+  if (res.moved) grid.addRandomTile();
+  return res;
 }
 
-// performs a search and returns the best move
+// Fonction principale qui effectue une recherche et retourne le meilleur mouvement
 function AI_getBest(grid, debug) {
-	var runs = document.getElementById('run-count').value;
-    return getBestMove(grid, runs, debug);  
+  var runs = document.getElementById('run-count').value;
+  return getBestMove(grid, runs, debug);  
 }
-
